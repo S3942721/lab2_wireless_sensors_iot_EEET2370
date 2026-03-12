185e9dd508ed6a33a1d656f0d2addcb7330b206a
Sam Griffiths <samueljfgriffiths@icloud.com>
2026-03-12 11:02:02 +1100

    initial commit

---
+++ LoRa_Simple_Server_Yun_RMIT/LoRa_Simple_Server_Yun_RMIT.ino
@@ -0,0 +1,70 @@
+#define BAUDRATE 115200
+
+#include <Console.h>
+#include <SPI.h>
+#include <RH_RF95.h>
+
+// Singleton instance of the radio driver
+RH_RF95 rf95;
+
+int led = A2;
+float frequency = 916.0;
+
+void setup() 
+{
+  pinMode(led, OUTPUT);     
+  Bridge.begin(BAUDRATE);
+  Console.begin();
+  while (!Console) ; // Wait for console port to be available
+  Console.println("Start Sketch");
+  if (!rf95.init())
+    Console.println("init failed");
+
+  rf95.setFrequency(frequency);
+  // Setup Power,dBm
+  rf95.setTxPower(3);
+  
+  // Setup Spreading Factor (6 ~ 12)
+  rf95.setSpreadingFactor(7);
+  
+  // Setup BandWidth, option: 7800,10400,15600,20800,31200,41700,62500,125000,250000,500000
+  rf95.setSignalBandwidth(125000);
+  
+  // Setup Coding Rate:5(4/5),6(4/6),7(4/7),8(4/8) 
+  rf95.setCodingRate4(5);
+  
+  Console.print("Listening on frequency: ");
+  Console.println(frequency);
+}
+
+void loop()
+{
+  if (rf95.available())
+  {
+    // Should be a message for us now   
+    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+    uint8_t len = sizeof(buf);
+    if (rf95.recv(buf, &len))
+    {
+      digitalWrite(led, HIGH);
+      RH_RF95::printBuffer("request: ", buf, len);
+      Console.print("got request: ");
+      Console.println((char*)buf);
+      Console.print("RSSI: ");
+      Console.println(rf95.lastRssi(), DEC);
+      
+      // Send a reply
+      uint8_t data[] = "And hello back to you Group 1";
+      rf95.send(data, sizeof(data));
+      rf95.waitPacketSent();
+      Console.println("Sent a reply");
+      digitalWrite(led, LOW);
+    }
+    else
+    {
+      Console.println("recv failed");
+    }
+  }
+}
+
+
